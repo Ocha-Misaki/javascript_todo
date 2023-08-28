3b34e9588c63984130c649824e031115457fe7f7
Ocha-Misaki <himawari.158fuji@gmail.com>
2023-08-28 16:00:48 +0900

    addした際にXボタンが表示されるようにした

--- main.js
+++ main.js
@@ -12,7 +12,6 @@
       this.obj.id = 2
       this.obj.title = title
       this.obj.isCompleted = false
-      debugger
       this.todos.push(this.obj)
       localStorage.setItem('todos',this.todos)
     }
@@ -21,19 +20,20 @@
       this.todosElement = document.getElementById('todos')
       this.label = document.createElement('label')
       this.input = document.createElement('input')
-      this.title = document.createTextNode(todo.todos)
       this.input.type = 'checkbox'
-      
+      this.title = document.createTextNode(todo.todos)
+      this.deleteButton = document.createElement('button')
+      this.deleteButton.textContent = 'X'
+      this.deleteButton.setAttribute('id','delete')
       this.label.appendChild(this.input)
       this.label.appendChild(this.title)
+      this.label.appendChild(this.deleteButton)
       this.todosElement.appendChild(this.label)
       
       
         this.title.textContent = this.obj.title
       
     }
-
-    
   }
   const todo = new ToDo()
 
