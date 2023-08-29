4695c90c2dec1bc4fc62247dc67005269c55b06c
Ocha-Misaki <himawari.158fuji@gmail.com>
2023-08-29 16:31:22 +0900

    リロードした際もlocalstorageからデータを取得しcheckboxを表示できるようにした

--- main.js
+++ main.js
@@ -4,39 +4,46 @@
     constructor(elementId){
       this.todos = []
       const string = localStorage.getItem('todos')
+
       if(string !== null){
-        this.todos = string.split(',')
+        this.todos = JSON.parse(string)
       }
       this.elementId = elementId
     }
 
     add(title){
-    
       this.createCheckbox(title)
       this.todos.push({id: 2, title: title, isCompleted: false})
       localStorage.setItem('todos', JSON.stringify(this.todos))
     }
 
     createCheckbox(title){
-      this.todosElement = document.getElementById(this.elementId)
-      this.label = document.createElement('label')
-      this.input = document.createElement('input')
-      this.input.type = 'checkbox'
-      this.title = document.createTextNode(todo.todos)
-      this.deleteButton = document.createElement('button')
-      this.deleteButton.textContent = 'X'
-      this.deleteButton.setAttribute('id','delete')
-      this.label.appendChild(this.input)
-      this.label.appendChild(this.title)
-      this.label.appendChild(this.deleteButton)
-      this.todosElement.appendChild(this.label)
+      const taskCheckbox = document.createElement('input')
+      taskCheckbox.type = 'checkbox'
       
-      this.title.textContent = title
+      const taskTitle = document.createTextNode(title)
+      // taskTitle.textContent = title
       
+      const deleteButton = document.createElement('button')
+      deleteButton.textContent = 'X'
+      deleteButton.setAttribute('id','delete')
+      
+      const taskLabel = document.createElement('label')
+      taskLabel.appendChild(taskCheckbox)
+      taskLabel.appendChild(taskTitle)
+      taskLabel.appendChild(deleteButton)
+      
+      const todosElement = document.getElementById(this.elementId)
+      todosElement.appendChild(taskLabel)
     }
   }
+  
   const todo = new ToDo('todos')
-
+  const todos = todo.todos
+  todos.forEach(t => {
+    todo.createCheckbox(t.title)
+  })
+  
   const form = document.getElementById('todo_form')
   const addButton = document.getElementById('addButton')
   addButton.addEventListener('click', () => {
