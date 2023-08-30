9a8d5d281d84104289383bb0ce74057e5399925c
Ocha-Misaki <himawari.158fuji@gmail.com>
2023-08-30 16:11:48 +0900

    formatを整えた

--- main.js
+++ main.js
@@ -1,39 +1,38 @@
 'use strict'
 {
   class ToDo {
-    constructor(elementId){
+    constructor(elementId) {
       this.todos = []
       const string = localStorage.getItem('todos')
 
-      if(string !== null){
+      if (string !== null) {
         this.todos = JSON.parse(string)
       }
       this.elementId = elementId
     }
 
-    
-    add(title){
-      getMaxId()
-      const task = {id: generateTaskId(), title: title, isCompleted: false}
+    add(title) {
+      const task = { id: generateTaskId(), title: title, isCompleted: false }
       this.todos.push(task)
       localStorage.setItem('todos', JSON.stringify(this.todos))
       this.createCheckbox(title)
     }
 
-    createCheckbox(title){
+    createCheckbox(title) {
       const taskCheckbox = document.createElement('input')
       taskCheckbox.type = 'checkbox'
-      taskCheckbox.addEventListener('change',() => {
-        if(taskCheckbox.checked === true){
+      taskCheckbox.addEventListener('change', () => {
+        if (taskCheckbox.checked === true) {
+          console.log(taskCheckbox)
           //isCompleted = trueのようにしたい
+          //削除のスタイルが当たるようにする
         }
       })
-      
-      
+
       const taskTitle = document.createTextNode(title)
       const deleteButton = document.createElement('button')
       deleteButton.textContent = 'X'
-      deleteButton.setAttribute('id','delete')
+      deleteButton.setAttribute('id', 'delete')
       const taskLabel = document.createElement('label')
       taskLabel.appendChild(taskCheckbox)
       taskLabel.appendChild(taskTitle)
@@ -42,25 +41,26 @@
       todosElement.appendChild(taskLabel)
     }
   }
-  
+
   const todo = new ToDo('todos')
   const todos = todo.todos
-  todos.forEach(t => {
+  todos.forEach((t) => {
     todo.createCheckbox(t.title)
   })
 
-  let maxId;
+  let maxId
   const generateTaskId = () => {
     const idArray = []
-      todos.forEach(t => {
-        idArray.push(t.id)
-      })
-      if(idArray.length == 0){
-        maxId = 0
-      } else {
-        maxId = Math.max(...idArray)
-      }
-    // return todos.length === 0 ? 0: Math.max(todos.map(task => task.id))
+    todos.forEach((t) => {
+      idArray.push(t.id)
+    })
+    if (idArray.length == 0) {
+      maxId = 0
+    } else {
+      maxId = Math.max(...idArray)
+    }
+
+    // return todos.length === 0 ? 0 : Math.max(todos.map(task => task.id))
     //↑の実装について確認する
   }
 
@@ -69,4 +69,8 @@
   addButton.addEventListener('click', () => {
     todo.add(form.value)
   })
+
+  //deleteボタンを押した時の実装を作成する
+
+  //purgeボタンを押した時の実装を作成する
 }
